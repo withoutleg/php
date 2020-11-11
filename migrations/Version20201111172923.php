0e994baf33921f494cd011915f0e0a188fe38e90
diman <withoutleg@gmail.com>
2020-11-11 20:55:30 +0300

    migration

---
+++ migrations/Version20201111172923.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201111172923 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE establishment (id INT NOT NULL, name VARCHAR(255) NOT NULL, latitude INT DEFAULT NULL, longitude INT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX search_idx ON establishment (latitude, longitude)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE establishment');
+    }
+}
